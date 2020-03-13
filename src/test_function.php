ffaa223a50885f7f03706afea4ab65d02beb62d6
lionel <lionel.informatique.plugimmo@gmail.com>
2020-03-13 13:19:16 +0100

    test finish

--- src/test_function.php
+++ src/test_function.php
@@ -1,9 +1,8 @@
 <?php
 require 'API.php';
 
-if(file_exists("conf.json"))
-{
-    $json = json_decode(file_get_contents("conf.json"),true);
+if (file_exists("conf.json")) {
+    $json = json_decode(file_get_contents("conf.json"), true);
     $url = $json['URL'];
     $login = $json['LOGIN'];
     $password = $json['MDP'];
@@ -13,19 +12,18 @@ if(file_exists("conf.json"))
     $api->login("POST", "/key/");
     $programs = json_decode($api->getFromEndpoint("/programs/"), true);
     $fp = fopen('programs.json', 'w+');
-
+    $program_json = array();
 
     foreach ($programs as $program) {
-        $program_infos = $api->getFromEndpoint("/programs/" . $program["programId"]."/");
-        fwrite($fp, json_encode(
-            json_decode(
-                $program_infos))
-        );
-
+        $program_json[] = json_decode($api->getFromEndpoint("/programs/" . $program["programId"] . "/"), true);
     }
 
+    fwrite($fp, json_encode(
+            $program_json, JSON_UNESCAPED_UNICODE)
+    );
+
     fclose($fp);
-}else{
+} else {
     echo "Fichier de configuration inexistant";
 }
 
